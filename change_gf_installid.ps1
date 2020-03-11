a18ec20cd05581f446b22f91c32ca02d56ae75ab
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2020-03-11 16:37:09 +0100

    Update change_gf_installid.ps1

--- change_gf_installid.ps1
+++ change_gf_installid.ps1
@@ -2,6 +2,7 @@ param (
     [string]$newid = ""
  )
 
-Set-Itemproperty -path 'HKLM:\SOFTWARE\WOW6432Node\Gameforge4d\TNTClient\MainApp' -name 'InstallationId' -value "$newid"
-Set-Itemproperty -path 'HKLM:\SOFTWARE\WOW6432Node\Gameforge4d\GameforgeClient\MainApp' -name 'InstallationId' -value "$newid"
-Set-Itemproperty -path 'HKCU:\Software\Gameforge4d\TNTClient\MainApp' -name 'InstallationId' -value "$newid"
+Set-Itemproperty -path 'HKLM:\SOFTWARE\WOW6432Node\Gameforge4d\TNTClient\MainApp' -name 'InstallationId' -value "$newid" *>$null
+Set-Itemproperty -path 'HKLM:\SOFTWARE\WOW6432Node\Gameforge4d\GameforgeClient\MainApp' -name 'InstallationId' -value "$newid" *>$null
+Set-Itemproperty -path 'HKCU:\Software\Gameforge4d\TNTClient\MainApp' -name 'InstallationId' -value "$newid" *>$null
+Set-Itemproperty -path 'HKCU:\Software\Gameforge4d\GameforgeClient\MainApp' -name 'InstallationId' -value "$newid" *>$null
