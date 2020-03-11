5774bc209c6ac578fb379f43f1725e64e0111a56
Gilgames000 <22778436+Gilgames000@users.noreply.github.com>
2020-03-11 15:49:42 +0100

    Add powershell script to change install ID

---
+++ change_gf_installid.ps1
@@ -0,0 +1,7 @@
+param (
+    [string]$newid = ""
+ )
+
+Set-Itemproperty -path 'HKLM:\SOFTWARE\WOW6432Node\Gameforge4d\TNTClient\MainApp' -name 'InstallationId' -value "$newid"
+Set-Itemproperty -path 'HKLM:\SOFTWARE\WOW6432Node\Gameforge4d\GameforgeClient\MainApp' -name 'InstallationId' -value "$newid"
+Set-Itemproperty -path 'HKCU:\Software\Gameforge4d\TNTClient\MainApp' -name 'InstallationId' -value "$newid"
